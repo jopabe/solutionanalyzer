a2b2c595f412a56d8c200dff06489daba84a62fa
Joris Patroons <jopa@jox.be>
2022-01-30 15:10:33 +0100

    initial version

---
+++ src/Jox.SolutionAnalyzer/Printer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Construction;
+
+internal class Printer
+{
+    internal static async Task PrintSolutionAsync(SolutionFile sln, string slnPath)
+    {
+        foreach (var proj in sln.ProjectsInOrder)
+        {
+            Console.WriteLine($"{slnPath};{proj.ProjectType};{proj.ProjectName};{proj.AbsolutePath}");
+        }
+        await Task.Delay(100);
+    }
+}
---
+++ src/Jox.SolutionAnalyzer/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Construction;
+using System.CommandLine;
+
+var solutionFile = new Argument<FileInfo[]>("solutionFile", "The solution file") { Arity = ArgumentArity.OneOrMore };
+
+var rootCommand = new RootCommand("Analyze a .NET solution for projects and dependencies")
+{
+    solutionFile
+};
+
+rootCommand.SetHandler(async (FileInfo[] slnFiles) =>
+{
+    foreach (FileInfo slnFile in slnFiles)
+    {
+        var sln = SolutionFile.Parse(slnFile.FullName);
+        await Printer.PrintSolutionAsync(sln, slnFile.FullName);
+    }
+}, solutionFile);
+
+
+return rootCommand.Invoke(args);
